2bd23a04257d7ae1ce48ed3cf07970aef59df91c
el20mgam <91559622+el20mgam@users.noreply.github.com>
2022-04-14 15:52:12 +0100

    Update _1HZ_BLINK.v

--- _1HZ_BLINK.v
+++ _1HZ_BLINK.v
@@ -1,7 +1,7 @@
 module _1HZ_BLINK (
    input clk_50MHz,
    input set_n,
-   output clk_1Hz
+   output reg clk_1Hz = 0
 	
    );
  
@@ -11,22 +11,16 @@ parameter div_1HZ = 100000000;
 //The counter:
 reg [31:0] count_1HZ = 0;
    
-//The frequency register:
-reg r_1HZ;
-   
 //Lowers Clock signal to 1HZ 
   always @ (posedge clk_50MHz)
     begin
       if (count_1HZ == div_1HZ-1)
         begin        
-          r_1HZ <= !r_1HZ;
+          clk_1Hz <= !clk_1Hz;
           count_1HZ    <= 0;
         end
       else
         count_1HZ <= count_1HZ + 1;
     end
-
-//Ouput the clock singal or 0 if set_n is on 
-assign clk_1Hz = r_1HZ & set_n;
 
 endmodule
